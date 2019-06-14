2488f5e72a6998397b9e5d3233b02870a7492621
Sumana Basu <sumanab@fb.com>
2019-06-14 11:37:50 -0400

    options added for reconstructor model parameters

--- options/train_options.py
+++ options/train_options.py
@@ -56,4 +56,13 @@ class TrainOptions(BaseOptions):
         # ########################
         parser.add_argument('--validation_train_split_ratio', type=float, default=0.9)
 
+        # Options for Reconstruction Model
+        parser.add_argument('--number_of_filters', type=int, default=128)
+        parser.add_argument('--dropout_probability', type=float, default=0)
+        parser.add_argument('--number_of_cascade_blocks', type=int, default=3)
+        parser.add_argument('--number_of_layers_residual_bottleneck', type=int, default=6)
+        parser.add_argument('--mask_embed_dim', type=int, default=6)
+        parser.add_argument('--n_downsampling', type=int, default=3)
+        parser.add_argument('--use_deconv', type=bool, default=True)
+
         return parser
