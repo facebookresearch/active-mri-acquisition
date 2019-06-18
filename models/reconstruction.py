5021a526501e64fbb50148f969325b08964af5c5
Sumana Basu <sumanab@fb.com>
2019-06-17 20:25:46 -0400

    minor changes in comment

--- models/reconstruction.py
+++ models/reconstruction.py
@@ -384,7 +384,7 @@ class ReconstructorNetwork(nn.Module):
                                         shape = (batch_size, 1, 1, width)
 
         Returns:    reconstructed high resolution image
-                    uncertainy map
+                    uncertainty map
                     mask_embedding
 
         '''
