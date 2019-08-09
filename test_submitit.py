e066b9daefc3899b9016ba86f74a192b3491ef3a
Luis Pineda <lep@fb.com>
2019-08-09 15:13:37 -0400

    added script to run multiple dqn in cluster. other minor changes

--- test_submitit.py
+++ test_submitit.py
@@ -6,6 +6,7 @@ from hyperband import HyperparametersConfig
 
 import torch
 
+
 def main(options):
     # Create a function evaluator to be passed to the tuner. Here you can pass the
     # SLURM arguments as keywords.
@@ -16,9 +17,10 @@ def main(options):
         3,
         job_name='submitit_test',
         time=4320,
-        partition='learnfair',
+        partition='priority',
         num_gpus=8,
-        cpus_per_task=16)
+        cpus_per_task=16,
+        comment='short job debugging stuff')
 
     hp = ['lambda_gan']
 
@@ -27,7 +29,14 @@ def main(options):
     # hp_config.assign('mask_embed_dim', 6)
     hp_config.assign('lambda_gan', 0)
 
-    function_evaluator.submit_for_evaluation(hp_config, resource_budget=50)
+    jobid = function_evaluator.submit_for_evaluation(hp_config, resource_budget=50)
+    print(jobid)
+
+    while True:
+        results = function_evaluator.get_any_results_for_ids([jobid])
+        if len(results) > 0:
+            break
+        print('No results', flush=True)
 
 
 if __name__ == '__main__':
@@ -35,4 +44,3 @@ if __name__ == '__main__':
     options.device = torch.device('cuda:{}'.format(
         options.gpu_ids[0])) if options.gpu_ids else torch.device('cpu')
     main(options)
-
