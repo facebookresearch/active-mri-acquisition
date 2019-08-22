fixed sign leakage in reconstructor/evaluator, tensorboard plots. removed unnecessary code

data/ft_data_loader/ft_util.py
@@ -4,7 +4,7 @@ import torch
 
 
 def kspace_to_image(kspace_tensor):
-    # return (1,H,W)   
+    # return (1,H,W)
     inv_img = torch.irfft(kspace_tensor, 2, onesided=False)
 
     return inv_img
@@ -18,13 +18,13 @@ def image_to_kspace(im_tensor):
 
 # this is easy to control than gen_kspace_mask_deprecated
 def gen_kspace_mask(shape, ratio, lowfreq_ratio):
-    
+
     h, w = shape
     mask_low_freqs = int(np.ceil(int(lowfreq_ratio * h) / 2))
-    frac = ratio - mask_low_freqs*2/h
+    frac = ratio - mask_low_freqs * 2 / h
     mask_fft = (np.random.RandomState(42).rand(h) < frac).astype(np.float32)
     mask_fft[:mask_low_freqs] = mask_fft[-mask_low_freqs:] = 1
-    mask_fft = torch.from_numpy(mask_fft).view(1, h, 1, 1).expand(1,h,w,2)
+    mask_fft = torch.from_numpy(mask_fft).view(1, h, 1, 1).expand(1, h, w, 2)
 
     return mask_fft
 
@@ -36,107 +36,104 @@ def gen_kspace_mask_deprecated(shape, ratio, subsampling_type='lf_focused'):
     """
     #pdb.set_trace()
     np.random.seed(0)
-    img_mask_np = np.zeros(shape, np.float32) #img_fft_np.copy()
+    img_mask_np = np.zeros(shape, np.float32)  #img_fft_np.copy()
     nrows = shape[1]
 
     center_fraction = 0.1
-    
+
     # Just simplfies the implementation
     if not float(ratio).is_integer():
         raise Exception("Subsampling ratio should be integral")
 
     # For random methods
-    nrows_keep = int(nrows/ratio)
+    nrows_keep = int(nrows / ratio)
     inds = np.random.choice(nrows, size=nrows_keep, replace=False)
 
     # For center masking methods
-    radius = int(nrows*center_fraction/2)
-    middle = int(nrows/2)
+    radius = int(nrows * center_fraction / 2)
+    middle = int(nrows / 2)
     non_center_inds = (
-        list(range(radius, middle-radius)) +
-        list(range(middle+radius, nrows-radius)))
+        list(range(radius, middle - radius)) + list(range(middle + radius, nrows - radius)))
     non_center_subsample = list(set(inds) & set(non_center_inds))
 
     if subsampling_type == "random":
-        img_mask_np[:,inds,:,:] = 1
+        img_mask_np[:, inds, :, :] = 1
     elif subsampling_type == "hf_focused":
         # region around center of image should be all 1
         # region around either end should be all 0
-        img_mask_np[:,(middle-radius):(middle+radius),:,:] = 1
-        img_mask_np[:,non_center_subsample,:,:] = 1
+        img_mask_np[:, (middle - radius):(middle + radius), :, :] = 1
+        img_mask_np[:, non_center_subsample, :, :] = 1
     elif subsampling_type == "lf_focused":
         # region around center of image should be all 0
         # region around either end should be all 1
 
         ## Want low frequences, which are near the top and bottom
-        img_mask_np[:,:radius,:,:] = 1
-        img_mask_np[:,(-radius):,:,:] = 1
-        img_mask_np[:,non_center_subsample,:,:] = 1
+        img_mask_np[:, :radius, :, :] = 1
+        img_mask_np[:, (-radius):, :, :] = 1
+        img_mask_np[:, non_center_subsample, :, :] = 1
     elif subsampling_type == "alternating_plus_hf":
-        img_mask_np[:,::ratio,:,:] = 1
-        img_mask_np[:,(middle-radius):(middle+radius),:,:] = 1
-        img_mask_np[:,:radius,:,:] = 0
-        img_mask_np[:,(-radius):,:,:] = 0
+        img_mask_np[:, ::ratio, :, :] = 1
+        img_mask_np[:, (middle - radius):(middle + radius), :, :] = 1
+        img_mask_np[:, :radius, :, :] = 0
+        img_mask_np[:, (-radius):, :, :] = 0
     elif subsampling_type == "alternating_plus_lf":
         #https://arxiv.org/pdf/1709.02576.pdf
-        img_mask_np[:,::ratio,:,:] = 1
-        img_mask_np[:,(middle-radius):(middle+radius),:,:] = 0
-        img_mask_np[:,:radius,:,:] = 1
-        img_mask_np[:,(-radius):,:,:] = 1
+        img_mask_np[:, ::ratio, :, :] = 1
+        img_mask_np[:, (middle - radius):(middle + radius), :, :] = 0
+        img_mask_np[:, :radius, :, :] = 1
+        img_mask_np[:, (-radius):, :, :] = 1
     elif subsampling_type == "alternating":
         # Really doesn'y work very well!
-        img_mask_np[:,::ratio,:,:] = 1
+        img_mask_np[:, ::ratio, :, :] = 1
 
     return img_mask_np
 
 
-class FourierUtil():
-    def __init__(self, unmask_ratio, normalize, low_freq_portion=0.8):
-        # unmask_ratio: kspace overall mask ratio
-        # normalize: if images to the input of _to_kspace is [-1, 1] normalized
-        # low_freq_portion: what is the porportion of unmask_ratio to be low frequency
-
-        self.unmask_ratio = unmask_ratio
-        # self.mask_type = mask_type
-        self.tanh_norm = normalize # if need to normalize to 0-1 (usually for gan)
-        # we want low_freq_portion% are low frequency
-        if low_freq_portion < 0.6 :
-            print('Warning: low_frequency portion is low. It may causes images look bad')
-        self.lowfreq_ratio = unmask_ratio * low_freq_portion
-       
-    def _to_kspace(self, img):
-
-        if not hasattr(self, 'mask'):
-            self.img_size = list(img.shape)
-            ## old way in mri_deep_prior code
-            # mask_np = gen_kspace_mask(self.img_size+[2], ratio=1//self.unmask_ratio, subsampling_type=self.mask_type)
-            # self.mask = torch.from_numpy(mask_np)
-            ## new way in anuroop's code
-            self.mask = gen_kspace_mask(self.img_size[1:], ratio=self.unmask_ratio, lowfreq_ratio=self.lowfreq_ratio)
-            
-
-        if self.tanh_norm:
-            # first put back image to [0-1]
-            img = img.add(1).div(2)
-
-        kspace_complex = image_to_kspace(img)
-        # mask kspace
-        masked_kspace = kspace_complex * self.mask
-
-        rec_img = kspace_to_image(masked_kspace)
-        #TODO do we need to recale rec_img
-        rec_img.add_(-rec_img.min()).div_(rec_img.max() - rec_img.min())
-        if self.tanh_norm:
-            rec_img.mul_(2).add_(-1)
-        
-        _masked_kspace = kspace_complex * (1 - self.mask) # inverse of masked kspace
-        _rec_img = kspace_to_image(_masked_kspace)
-        _rec_img.add_(-_rec_img.min()).div_(_rec_img.max() - _rec_img.min())
-        if self.tanh_norm:
-            _rec_img.mul_(2).add_(-1)
-
-        stacked_data = torch.cat([rec_img, _rec_img], dim=0) # 3x1xhxw
-
-        return stacked_data
-        
-
+# class FourierUtil():
+#     def __init__(self, unmask_ratio, normalize, low_freq_portion=0.8):
+#         # unmask_ratio: kspace overall mask ratio
+#         # normalize: if images to the input of _to_kspace is [-1, 1] normalized
+#         # low_freq_portion: what is the porportion of unmask_ratio to be low frequency
+#
+#         self.unmask_ratio = unmask_ratio
+#         # self.mask_type = mask_type
+#         self.tanh_norm = normalize # if need to normalize to 0-1 (usually for gan)
+#         # we want low_freq_portion% are low frequency
+#         if low_freq_portion < 0.6 :
+#             print('Warning: low_frequency portion is low. It may causes images look bad')
+#         self.lowfreq_ratio = unmask_ratio * low_freq_portion
+#
+#     def _to_kspace(self, img):
+#
+#         if not hasattr(self, 'mask'):
+#             self.img_size = list(img.shape)
+#             ## old way in mri_deep_prior code
+#             # mask_np = gen_kspace_mask(self.img_size+[2], ratio=1//self.unmask_ratio, subsampling_type=self.mask_type)
+#             # self.mask = torch.from_numpy(mask_np)
+#             ## new way in anuroop's code
+#             self.mask = gen_kspace_mask(self.img_size[1:], ratio=self.unmask_ratio, lowfreq_ratio=self.lowfreq_ratio)
+#
+#
+#         if self.tanh_norm:
+#             # first put back image to [0-1]
+#             img = img.add(1).div(2)
+#
+#         kspace_complex = image_to_kspace(img)
+#         # mask kspace
+#         masked_kspace = kspace_complex * self.mask
+#
+#         rec_img = kspace_to_image(masked_kspace)
+#         #TODO do we need to recale rec_img
+#         rec_img.add_(-rec_img.min()).div_(rec_img.max() - rec_img.min())
+#         if self.tanh_norm:
+#             rec_img.mul_(2).add_(-1)
+#
+#         _masked_kspace = kspace_complex * (1 - self.mask) # inverse of masked kspace
+#         _rec_img = kspace_to_image(_masked_kspace)
+#         _rec_img.add_(-_rec_img.min()).div_(_rec_img.max() - _rec_img.min())
+#         if self.tanh_norm:
+#             _rec_img.mul_(2).add_(-1)
+#
+#         stacked_data = torch.cat([rec_img, _rec_img], dim=0) # 3x1xhxw
+#
+#         return stacked_data

data/ft_data_loader/ft_util_vaes.py
@@ -389,18 +389,19 @@ class DicomDataTransform:
         return mask, image
 
     def postprocess(self, data, hps):
-        inputs = data[1]
-        mask = data[0].repeat(1, 1, hps.resolution, 1)
-        inputs = (inputs.clamp_(-2, 4) + 2) / 6
-        inputs = torch.cat((inputs, torch.zeros(inputs.shape)), 1)
-        inputs = inputs.cuda()
-        mask = mask.cuda()
-        mask_k_space = mask
-        mask = mask.unsqueeze(4).repeat(1, 1, 1, 1, 2)
-        kspace = torch.fft(inputs.permute(0, 2, 3, 1), 2, normalized=False).unsqueeze(1)
-        masked_kspace = kspace * mask
-        masked_image = torch.ifft(masked_kspace, 2, normalized=False).squeeze(1).permute(0, 3, 1, 2)
-        return inputs, masked_image, mask_k_space
+        raise NotImplementedError('Need to fix sign leakage here')
+        # inputs = data[1]
+        # mask = data[0].repeat(1, 1, hps.resolution, 1)
+        # inputs = (inputs.clamp_(-2, 4) + 2) / 6
+        # inputs = torch.cat((inputs, torch.zeros(inputs.shape)), 1)
+        # inputs = inputs.cuda()
+        # mask = mask.cuda()
+        # mask_k_space = mask
+        # mask = mask.unsqueeze(4).repeat(1, 1, 1, 1, 2)
+        # kspace = torch.fft(inputs.permute(0, 2, 3, 1), 2, normalized=False).unsqueeze(1)
+        # masked_kspace = kspace * mask
+        # masked_image = torch.ifft(masked_kspace, 2, normalized=False).squeeze(1).permute(0, 3, 1, 2)
+        # return inputs, masked_image, mask_k_space
 
 
 class RawDataTransform:

models/fft_utils.py
@@ -2,12 +2,13 @@ import warnings
 
 import numpy as np
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
 
 # note that for IFFT we do not use irfft
 # this function returns two channels where the first one (real part) is in image space
+from torch import nn as nn
+from torch.nn import functional as F
+
+
 class IFFT(nn.Module):
 
     def forward(self, x, normalized=False):
@@ -88,7 +89,8 @@ def preprocess_inputs(target,
         target = torch.norm(
             target, p=2, dim=3, keepdim=True)  #TODO: to be updated based on decision
         target = target.permute(0, 3, 1, 2)
-    masked_true_k_space = fft_functions['rfft'](target) * mask
+    fft_target = fft_functions['rfft'](target)
+    masked_true_k_space = torch.where(mask.byte(), fft_target, torch.tensor(0.).to(options.device))
     zero_filled_reconstruction = fft_functions['ifft'](masked_true_k_space)
 
     target = torch.cat([target, torch.zeros_like(target)], dim=1)
@@ -134,3 +136,70 @@ def create_mask(batch_size, num_entries=128, mask_type='random', low_freq_count=
             raise ValueError('Invalid mask type: {}.'.format(mask_type))
 
     return torch.from_numpy(mask).view(batch_size, 1, 1, num_entries)
+
+
+class GANLossKspace(nn.Module):
+
+    def __init__(self, use_lsgan=True, use_mse_as_energy=False, grad_ctx=False, gamma=100):
+        super(GANLossKspace, self).__init__()
+        # self.register_buffer('real_label', torch.ones(imSize, imSize))
+        # self.register_buffer('fake_label', torch.zeros(imSize, imSize))
+        self.grad_ctx = grad_ctx
+        if use_lsgan:
+            self.loss = nn.MSELoss(size_average=False)
+        else:
+            self.loss = nn.BCELoss(size_average=False)
+        self.use_mse_as_energy = use_mse_as_energy
+        if use_mse_as_energy:
+            self.RFFT = RFFT()
+            self.gamma = gamma
+            self.bin = 5
+
+    def get_target_tensor(self, input, target_is_real, degree, mask, pred_and_gt=None):
+
+        if target_is_real:
+            target_tensor = torch.ones_like(input)
+            target_tensor[:] = degree
+
+        else:
+            target_tensor = torch.zeros_like(input)
+            if not self.use_mse_as_energy:
+                if degree != 1:
+                    target_tensor[:] = degree
+            else:
+                pred, gt = pred_and_gt
+                w = gt.shape[2]
+                ks_gt = self.RFFT(gt[:, :1, :, :], normalized=True)
+                ks_input = self.RFFT(pred, normalized=True)
+                ks_row_mse = F.mse_loss(
+                    ks_input, ks_gt, reduce=False).sum(
+                        1, keepdim=True).sum(
+                            2, keepdim=True).squeeze() / (2 * w)
+                energy = torch.exp(-ks_row_mse * self.gamma)
+
+                # do some bin process
+                # import pdb; pdb.set_trace()
+                # energy = torch.floor(energy * 10 / self.bin) * self.bin / 10
+
+                target_tensor[:] = energy
+            # force observed part to always
+            for i in range(mask.shape[0]):
+                idx = torch.nonzero(mask[i, 0, 0, :])
+                target_tensor[i, idx] = 1
+        return target_tensor
+
+    def __call__(self, input, target_is_real, mask, degree=1, updateG=False, pred_and_gt=None):
+        # input [B, imSize]
+        # degree is the realistic degree of output
+        # set updateG to True when training G.
+        target_tensor = self.get_target_tensor(input, target_is_real, degree, mask, pred_and_gt)
+        b, w = target_tensor.shape
+        if updateG and not self.grad_ctx:
+            mask_ = mask.squeeze()
+            # maskout the observed part loss
+            masked_input = torch.where((1 - mask_).byte(), input, torch.tensor(0.).to(input.device))
+            masked_target = torch.where((1 - mask_).byte(), target_tensor,
+                                        torch.tensor(0.).to(input.device))
+            return self.loss(masked_input, masked_target) / (1 - mask_).sum()
+        else:
+            return self.loss(input, target_tensor) / (b * w)

trainer.py
@@ -16,8 +16,8 @@ from typing import Any, Dict, Tuple
 
 from data import create_data_loaders
 from models.evaluator import EvaluatorNetwork
-from models.fft_utils import RFFT, IFFT, clamp, preprocess_inputs, gaussian_nll_loss
-from models.networks import GANLossKspace  # TODO: maybe move GANLossKspace to a loss file?
+from models.fft_utils import RFFT, IFFT, FFT, clamp, preprocess_inputs, gaussian_nll_loss
+from fft_utils import GANLossKspace
 from models.reconstruction import ReconstructorNetwork
 from options.train_options import TrainOptions
 from util import util
@@ -76,6 +76,7 @@ class Trainer:
         self.options = options
         self.best_validation_score = -float('inf')
         self.completed_epochs = 0
+        self.updates_performed = 0
 
         criterion_gan = GANLossKspace(
             use_mse_as_energy=options.use_mse_as_disc_energy, grad_ctx=options.grad_ctx).to(
@@ -83,7 +84,11 @@ class Trainer:
 
         self.losses = {'GAN': criterion_gan, 'NLL': gaussian_nll_loss}
 
-        self.fft_functions = {'rfft': RFFT().to(options.device), 'ifft': IFFT().to(options.device)}
+        self.fft_functions = {
+            'rfft': RFFT().to(options.device),
+            'ifft': IFFT().to(options.device),
+            'fft': FFT().to(options.device)
+        }
 
         if not os.path.exists(options.checkpoints_dir):
             os.makedirs(options.checkpoints_dir)
@@ -98,6 +103,7 @@ class Trainer:
             if self.options.use_evaluator else None,
             'completed_epochs': self.completed_epochs,
             'best_validation_score': self.best_validation_score,
+            'updates_performed': self.updates_performed
         }
 
     def get_loaders(self) -> Tuple[torch.utils.data.DataLoader, torch.utils.data.DataLoader]:
@@ -154,6 +160,7 @@ class Trainer:
                 self.optimizers['G'].load_state_dict(checkpoint['optimizer_G'])
                 self.completed_epochs = checkpoint['completed_epochs']
                 self.best_validation_score = checkpoint['best_validation_score']
+                self.updates_performed = checkpoint['updates_performed']
 
     def load_weights_from_given_checkpoint(self):
         if self.options.weights_checkpoint is None or not os.path.exists(
@@ -214,6 +221,8 @@ class Trainer:
 
         loss_G.backward()
         optimizers['G'].step()
+
+        self.updates_performed += 1
 
         return {'loss_D': 0 if self.evaluator is None else loss_D.item(), 'loss_G': loss_G.item()}
 
@@ -296,44 +305,45 @@ class Trainer:
 
         # Tensorboard Plots
         @train_engine.on(Events.ITERATION_COMPLETED)
-        def plot_training_loss(engine):
+        def plot_training_loss(_):
             writer.add_scalar("training/generator_loss", train_engine.state.output['loss_G'],
-                              engine.state.iteration)
+                              self.updates_performed)
             writer.add_scalar("training/discriminator_loss", train_engine.state.output['loss_D'],
-                              engine.state.iteration)
+                              self.updates_performed)
 
         @train_engine.on(Events.EPOCH_COMPLETED)
-        def plot_validation_loss(engine):
-            writer.add_scalar("validation/MSE", val_engine.state.output['MSE'], engine.state.epoch)
+        def plot_validation_loss(_):
+            writer.add_scalar("validation/MSE", val_engine.state.output['MSE'],
+                              self.completed_epochs)
             writer.add_scalar("validation/SSIM", val_engine.state.output['SSIM'],
-                              engine.state.epoch)
+                              self.completed_epochs)
 
         @train_engine.on(Events.EPOCH_COMPLETED)
-        def plot_validation_images(engine):
+        def plot_validation_images(_):
             ground_truth = util.create_grid_from_tensor(val_engine.state.output['ground_truth'])
-            writer.add_image("validation_images/ground_truth", ground_truth, engine.state.epoch)
+            writer.add_image("validation_images/ground_truth", ground_truth, self.completed_epochs)
 
             zero_filled_image = util.create_grid_from_tensor(
                 val_engine.state.output['zero_filled_image'])
             writer.add_image("validation_images/zero_filled_image", zero_filled_image,
-                             engine.state.epoch)
+                             self.completed_epochs)
 
             reconstructed_image = util.create_grid_from_tensor(
                 val_engine.state.output['zero_filled_image'])
             writer.add_image("validation_images/reconstructed_image", reconstructed_image,
-                             engine.state.epoch)
+                             self.completed_epochs)
 
             uncertainty_map = util.gray2heatmap(
                 util.create_grid_from_tensor(val_engine.state.output['uncertainty_map'].exp()),
                 cmap='jet')
             writer.add_image("validation_images/uncertainty_map", uncertainty_map,
-                             engine.state.epoch)
+                             self.completed_epochs)
 
             difference = util.create_grid_from_tensor(
                 torch.abs(val_engine.state.output['ground_truth'] -
                           val_engine.state.output['reconstructed_image']))
             difference = util.gray2heatmap(difference, cmap='gray')
-            writer.add_image("validation_images/difference", difference, engine.state.epoch)
+            writer.add_image("validation_images/difference", difference, self.completed_epochs)
 
         train_engine.add_event_handler(
             Events.EPOCH_COMPLETED,