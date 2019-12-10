7ce492ab849ef67c787d08a35551963b3b1d2f3a
Luis Pineda <lep@fb.com>
2019-12-10 09:14:47 -0800

    minor script modifications

--- scripts/train_all_reconstructors.sh
+++ scripts/train_all_reconstructors.sh
@@ -2,7 +2,10 @@
 
 EXTRA_OPTIONS=--no_evaluator
 
-CHECKPOINTS_DIR=/checkpoint/${USER}/active_acq/smaller_dicom_dataset
+NUM_VOL_TRAIN=1000
+NUM_VOL_VAL=$((NUM_VOL_TRAIN / 10))
+CHECKPOINTS_DIR=/checkpoint/${USER}/active_acq/smaller_dicom_dataset/num_vol_train_${NUM_VOL_TRAIN}
+
 
 #SRC_DIR=/private/home/lep/code/versions/Active_Acquisition/train_reconstructors_$(date +%Y%m%d_%H.%M.%S)
 SRC_DIR=/private/home/lep/code/versions/Active_Acquisition/train_reconstructors_dicom_smaller
@@ -24,7 +27,7 @@ for mask in basic_rnl symmetric_basic_rnl low_to_high_rnl; do
     --batchSize 40 \
     --gpu_ids 0,1,2,3,4,5,6,7 --print_freq 50 --lr 0.0006 --grad_ctx --max_epochs 100 \
     --print_freq 200 --use_mse_as_disc_energy --lambda_gan 0.1 \
-    --num_volumes_train 1000 \
-    --num_volumes_val 100 \
+    --num_volumes_train ${NUM_VOL_TRAIN} \
+    --num_volumes_val ${NUM_VOL_VAL} \
     --save_latest_freq 2000 ${EXTRA_OPTIONS} &
 done
