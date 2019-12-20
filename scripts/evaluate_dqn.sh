585c2124d9eac5434b8905f0ea641dc2ba638bc1
Luis Pineda <lep@fb.com>
2019-12-20 09:46:28 -0500

    some changes to evaluation script for dqns

--- scripts/evaluate_dqn.sh
+++ scripts/evaluate_dqn.sh
@@ -8,11 +8,12 @@ mkdir -p ${LOGS_DIR}/stdout
 mkdir -p ${LOGS_DIR}/stderr
 
 CHECKPOINTS_BASE=/checkpoint/lep/active_acq
-MODELS_DIR=${CHECKPOINTS_BASE}/all_reconstructors_post_eval_tag
+#MODELS_DIR=${CHECKPOINTS_BASE}/all_reconstructors_post_eval_tag
+MODELS_DIR=${CHECKPOINTS_BASE}/smaller_dicom_dataset/num_vol_train_1000
 MODEL_TYPE=symmetric_basic_rnl
 
 #SRC_DIR=/private/home/lep/code/versions/Active_Acquisition/test_dqn_$(date +%Y%m%d_%H.%M.%S)
-SRC_DIR=/private/home/lep/code/versions/Active_Acquisition/eval_many_dqns
+SRC_DIR=/private/home/lep/code/versions/Active_Acquisition/eval_dqns_smaller_dicom
 #rm -rf ${SRC_DIR}
 #mkdir -p ${SRC_DIR}
 #cp -r /private/home/lep/code/Active_Acquisition/* ${SRC_DIR}
@@ -23,8 +24,8 @@ queue=dev
 INIT_LINES=5
 BASELINES_SUFFIX=init.num.lines.${INIT_LINES}
 
-DQN_STR=image_space.tupd5000.bs16.edecay500000.gamma0.0.lr0.0001.repbuf300000norepl1.nimgtr10000000.metricssim.usescoasrew0_bu35_seed0_neptest100
-SUBDIR=long_dqns
+DQN_STR=image_space.tupd5000.bs16.edecay1000000.0.gamma0.0.lr0.0001.repbuf200000norepl1.nimgtr1000000.metricmse.usescoasrew0_bu35_seed0_neptest100
+SUBDIR=dqn
 
 job_name=evaluate_dqn_activeacq__${MODEL_TYPE}__${DQN_STR}
 
@@ -50,7 +51,7 @@ echo "cd ${SRC_DIR}" >> ${SLURM}
 echo srun python acquire_rl.py --dataroot KNEE \
     --reconstructor_dir ${MODELS_DIR}/${MODEL_TYPE}\
     --evaluator_dir ${MODELS_DIR}/${MODEL_TYPE}/evaluator \
-    --checkpoints_dir ${MODELS_DIR}/${MODEL_TYPE}/eval_${SUBDIR}/${DQN_STR} \
+    --checkpoints_dir ${MODELS_DIR}/${MODEL_TYPE}/${SUBDIR}/eval/${DQN_STR} \
     --dqn_weights_dir ${MODELS_DIR}/${MODEL_TYPE}/${SUBDIR}/${DQN_STR} \
     --test_set test \
     --num_test_images 1000 \
@@ -61,6 +62,7 @@ echo srun python acquire_rl.py --dataroot KNEE \
     --obs_to_numpy \
     --budget 1000 \
     --policy dqn \
+    --mask_type symmetric_basic_rnl \
     --dqn_model_type evaluator \
     --dqn_only_test \
     --replay_buffer_size 0 >> ${SLURM}
