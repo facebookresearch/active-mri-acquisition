change clipping in the loss, fix normalization in raw data

data/RAW_data_loader.py
@@ -115,8 +115,10 @@ class RawDataTransform:
         image = torch.ifft(kspace, 2, normalized=False)
         image = ifftshift(image, dim=(0, 1))
         norm = torch.sqrt(image[..., 0]**2 + image[..., 1]**2).max()
-        image /= norm
-        kspace /= norm
+        # 5.637766165023095e-08, 7.072103529760345e-07, 5.471710210258607e-06
+        # normalize by the mean norm of training images.
+        image /= 7.072103529760345e-07
+        kspace /= 7.072103529760345e-07
         shape = np.array(kspace.shape)
         seed = int(1009 * image.sum().abs()) if self.fixed_seed is None and self.seed_per_image \
             else self.fixed_seed

models/fft_utils.py
@@ -56,8 +56,8 @@ def gaussian_nll_loss(reconstruction, target, logvar, options):
         target = center_crop(target, [320, 320])
         logvar = center_crop(logvar, [320, 320])
     l2 = F.mse_loss(reconstruction, target, reduce=False)
-    # Clip logvar to make variance in [0.01, 5], for numerical stability
-    logvar = logvar.clamp(-4.605, 1.609)
+    # Clip logvar to make variance in [0.0001, 5], for numerical stability
+    logvar = logvar.clamp(-9.2, 1.609)
     one_over_var = torch.exp(-logvar)
 
     assert len(l2) == len(logvar)