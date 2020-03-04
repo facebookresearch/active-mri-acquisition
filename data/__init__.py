a65428f4109fe9939c8f6d373f5644adf318381c
Luis Pineda <luisen.p@gmail.com>
2020-03-04 08:14:04 -0500

    removed pytorch_mssim folder, data/image_folder.py, other unncessary stuff in util/util.py

--- data/__init__.py
+++ data/__init__.py
@@ -25,28 +25,3 @@ def create_data_loaders(options, is_test=False):
             mask_type=options.mask_type,
             rnl_params=options.rnl_params)
         return test_loader
-
-
-def find_dataset_using_name(dataset_name):
-    # Given the option --dataset [datasetname],
-    # the file "datasets/datasetname_dataset.py"
-    # will be imported.
-    dataset_filename = "data." + dataset_name + "_dataset"
-    datasetlib = importlib.import_module(dataset_filename)
-
-    # In the file, the class called DatasetNameDataset() will
-    # be instantiated. It has to be a subclass of BaseDataset,
-    # and it is case-insensitive.
-    dataset = None
-    target_dataset_name = dataset_name.replace('_', '') + 'dataset'
-    for name, cls in datasetlib.__dict__.items():
-        if name.lower() == target_dataset_name.lower() \
-           and issubclass(cls, BaseDataset):
-            dataset = cls
-
-    if dataset is None:
-        print(f"In {dataset_filename}.py, there should be a subclass of BaseDataset with class "
-              f"name that matches {target_dataset_name} in lowercase.")
-        exit(0)
-
-    return dataset
--- util/util.py
+++ util/util.py
@@ -1,11 +1,9 @@
 from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+from skimage.measure import compare_ssim, compare_psnr
 import torch
 import torchvision.utils as tvutil
-import numpy as np
-from PIL import Image
-import os
-import matplotlib.pyplot as plt
-from skimage.measure import compare_ssim, compare_psnr
 
 
 def compute_ssims(xs, ys):
@@ -35,36 +33,6 @@ def compute_nmse(xs, ys):
     return np.linalg.norm(ys_numpy - xs.cpu().numpy())**2 / np.linalg.norm(ys_numpy)**2
 
 
-def sum_axes(input, axes=[], keepdim=False):
-    # mu2, logvar2 are prior
-    # probably some check for uniqueness of axes
-    if axes == -1:
-        axes = [i for i in range(1, len(input.shape))]
-
-    if keepdim:
-        for ax in axes:
-            input = input.sum(ax, keepdim=True)
-    else:
-        for ax in sorted(axes, reverse=True):
-            input = input.sum(ax)
-    return input
-
-
-def mri_denormalize(input_image, zscore=3):
-    if isinstance(input_image, torch.Tensor):
-        image_tensor = input_image.data
-    else:
-        return input_image
-    # do normalization first, since we working on fourier space. we need to clamp
-    for dat in input_image:
-        minv = max(-zscore, dat.min())
-        maxv = min(zscore, dat.max())
-        dat.clamp_(minv, maxv)
-        dat.add_(-minv).div_(maxv - minv)
-
-    return input_image
-
-
 # Converts a Tensor into an image array (numpy)
 # |imtype|: the desired type of the converted numpy array
 def tensor2im(input_image, imtype=np.uint8, renormalize=True):
@@ -87,8 +55,6 @@ def tensor2im(input_image, imtype=np.uint8, renormalize=True):
     if image_numpy.shape[0] == 1:
         image_numpy = np.tile(image_numpy, (3, 1, 1))
 
-    # image_numpy = (np.transpose(image_numpy, (1, 2, 0)) + 1) / 2.0 * 255.0
-    # image_numpy = np.transpose(image_numpy, (1, 2, 0))
     return image_numpy.astype(imtype)
 
 
@@ -102,10 +68,10 @@ def create_grid_from_tensor(tensor_of_images, num_rows=4):
     Returns:
 
     """
-    #take norm over real-imaginary dimension
+    # take norm over real-imaginary dimension
     # tensor_of_images = tensor_of_images.norm(dim=1, keepdim=True)
 
-    #make image grid
+    # make image grid
     tensor_grid = tvutil.make_grid(
         tensor_of_images, nrow=num_rows, normalize=True, scale_each=False)
     numpy_grid = tensor2im(tensor_grid, renormalize=False)
@@ -120,47 +86,3 @@ def gray2heatmap(grayimg, cmap='jet'):
     rgb_img = rgba_img[:, :, :, 0] * 255.0
     rgb_img = rgb_img.astype(np.uint8)
     return rgb_img
-
-
-def diagnose_network(net, name='network'):
-    mean = 0.0
-    count = 0
-    for param in net.parameters():
-        if param.grad is not None:
-            mean += torch.mean(torch.abs(param.grad.data))
-            count += 1
-    if count > 0:
-        mean = mean / count
-    print(name)
-    print(mean)
-
-
-def save_image(image_numpy, image_path):
-    image_pil = Image.fromarray(image_numpy)
-    image_pil.save(image_path)
-
-
-def print_numpy(x, val=True, shp=False):
-    x = x.astype(np.float64)
-    if shp:
-        print('shape,', x.shape)
-    if val:
-        x = x.flatten()
-        print('mean = %3.3f, min = %3.3f, max = %3.3f, median = %3.3f, std=%3.3f' % (np.mean(x),
-                                                                                     np.min(x),
-                                                                                     np.max(x),
-                                                                                     np.median(x),
-                                                                                     np.std(x)))
-
-
-def mkdirs(paths):
-    if isinstance(paths, list) and not isinstance(paths, str):
-        for path in paths:
-            mkdir(path)
-    else:
-        mkdir(paths)
-
-
-def mkdir(path):
-    if not os.path.exists(path):
-        os.makedirs(path)
