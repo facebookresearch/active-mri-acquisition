52dc9c7fcdaa5f821996aac57a3dcf169146d035
Luis Pineda <luisen.p@gmail.com>
2020-07-06 12:35:07 -0400

    changed docs folder structure

--- docs/conf.py
+++ docs/conf.py
@@ -17,7 +17,7 @@
 
 # -- Project information -----------------------------------------------------
 
-project = 'active_mri_acquisition'
+project = 'Active MRI Acquisition'
 copyright = '2020, Luis Pineda, Adriana Romero, Sumana Basu, Roberto Calandra, Michal Drozdzal'
 author = 'Luis Pineda, Adriana Romero, Sumana Basu, Roberto Calandra, Michal Drozdzal'
 
@@ -36,7 +36,7 @@ templates_path = ['_templates']
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
+exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 
 
 # -- Options for HTML output -------------------------------------------------
--- docs/index.rst
+++ docs/index.rst
@@ -1,9 +1,9 @@
-.. active_mri_acquisition documentation master file, created by
-   sphinx-quickstart on Mon Jul  6 12:08:11 2020.
+.. Active MRI Acquisition documentation master file, created by
+   sphinx-quickstart on Mon Jul  6 12:34:38 2020.
    You can adapt this file completely to your liking, but it should at least
    contain the root `toctree` directive.
 
-Welcome to active_mri_acquisition's documentation!
+Welcome to Active MRI Acquisition's documentation!
 ==================================================
 
 .. toctree::
